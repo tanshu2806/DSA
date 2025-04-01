024fa681f70594e879e1f3ec3fe92908f6322e93
tanshu2806 <tanshutanishk2806@gmail.com>
2025-04-01 22:50:32 +0530

    Ternary Operator

---
+++ Introduction to CPP/5.cpp
@@ -0,0 +1,13 @@
+//Ternary Operator
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 5;
+    int y = 10;
+
+    int result = (x>y)? x : y;
+
+    cout<<"The largest input is "<<result<<endl;
+}
