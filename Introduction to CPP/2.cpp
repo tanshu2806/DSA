1f439742ba2763e5d1572c28f758983067e3be34
tanshu2806 <tanshutanishk2806@gmail.com>
2025-04-01 11:47:16 +0530

    Input variation

--- Introduction to CPP/2.cpp
+++ Introduction to CPP/2.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    int age;
-    cout<<"Enter yout age : ";
-    cin>>age;
 
-    cout<<"Your age is "<<age<<".";
+
+    int age;
+    cout<<"Enter you age : ";
+    cin>>age;
+    cin.ignore();
+    cout<<"Your age is "<<age<<"."<<endl;
+
+    string name;
+    cout<<"What is your name? "<<endl;
+    cin>>name; //This only takes input till first space
+    cin.ignore();
+    //But user have to enter only first name.
+    cout<<"Your name is "<<name<<"."<<endl;
+
+    
+    string fullname;
+    cout<<"Enter your full name : "<<endl;
+    getline(cin,fullname);
+
+    cout<<"Your full name is "<<fullname<<"."<< endl;
 
     return 0;
 
