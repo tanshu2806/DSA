8c987cd48a6ade93a013c1bf86507fd62e3a9e0b
tanshu2806 <tanshutanishk2806@gmail.com>
2025-04-02 11:56:34 +0530

    Loops

---
+++ Introduction to CPP/6.cpp
@@ -0,0 +1,77 @@
+//Loops
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // //counting 1 to 10
+    // for(int i =1;i<=10;i++){
+    //     if(i==5){
+    //         continue;
+    //     }
+    //     cout<<i<<endl;
+    // }
+
+    // //counting 100 to 1
+    // for(int i =100; i>0;i--){
+    //     cout<<i<<" ";
+    //     if(i==40){
+    //         break;
+    //     }
+    // }
+
+    // //While 
+    // int i = 1;
+    // while (i<10){
+    //     cout<<i<<" ";
+    //     i = i+1;
+    // }
+
+    //Do While
+    // int i =0;
+    // do{
+    //     cout<<i<<" ";
+    //     i=i+1;
+    // }while(i<=5);
+
+    // //Counting 1 to 100
+    // for (int i = 1; i<=100; i++){
+    //     cout<<i<<" ";
+    // }
+
+    // //counting 100 to 1
+    // for (int i = 100 ; i>0 ; i--){
+    //     cout<<i<<" ";
+    // }
+
+    // //Name  * 50
+    // for (int i = 1 ; i<= 50;i++){
+    //     cout<<i<<". Ram  ";
+    // }
+
+    // //counting 0 to -10
+    // for (int i =0;i>=-10;i--){
+    //     cout<<i<<" ";
+    // } 
+
+    // //Multipication Table
+    // int a;
+    // cout<<"Enter your number  : ";
+    // cin>>a;
+
+    // for(int i = 1;i<=10;i++){
+    //     cout<<a*i<<endl;
+    // }
+
+    // //from A to Z
+    // for (char ch = 'A';ch<='Z';ch++){
+    //     cout<<ch<<" ";
+    // }
+
+    // //from a to z
+    // for (char ch = 'a';ch<='z';ch++){
+    //     cout<<ch<< " ";
+    // }
+
+    return 0;
+}
