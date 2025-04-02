7622d8a048a8e6609630e1d09489c7be55cdfe24
tanshu2806 <tanshutanishk2806@gmail.com>
2025-04-02 11:25:33 +0530

    2nd ternary operator

--- Introduction to CPP/5.cpp
+++ Introduction to CPP/5.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 
 int main(){
-    int x = 5;
-    int y = 10;
+    // int x = 5;
+    // int y = 10;
 
-    int result = (x>y)? x : y;
+    // int result = (x>y)? x : y;
 
-    cout<<"The largest input is "<<result<<endl;
+    // cout<<"The largest input is "<<result<<endl;
+
+    int age ;
+    cout<<"Enter your age : ";
+    cin>>age;
+
+    (age>=18)? cout<<"You are adult" : cout<<"You are minor";  
 }
