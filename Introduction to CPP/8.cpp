800d065b7cc99ed9b92cb0df92eec1705194ced2
tanshu2806 <tanshutanishk2806@gmail.com>
2025-04-07 11:57:17 +0530

    nested loop

---
+++ Introduction to CPP/8.cpp
@@ -0,0 +1,12 @@
+//Nested loop
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i =1; i<=3;i++){
+        for(int j = 1;j<=3;j++){
+            cout<<"i :"<<i <<", j : "<<j<<endl;
+        }
+    }
+    return 0;
+}
