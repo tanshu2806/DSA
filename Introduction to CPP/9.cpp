1284c714e1cb791f60ab41955d3495575fbebfc1
tanshu2806 <tanshutanishk2806@gmail.com>
2025-04-07 12:20:36 +0530

    Operator

---
+++ Introduction to CPP/9.cpp
@@ -0,0 +1,65 @@
+//Operators
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Increment and Decrement
+    // int a =5;
+    // cout<<a;
+    // cout<<a++;
+    // cout<<++a;
+    // cout<<--a;
+    // cout<<a--;
+
+    //Arithmetic
+    // int a = 10;
+    // int b = 5;
+
+    // cout<<(a+b)<<endl;
+    // cout<<(a-b)<<endl;
+    // cout<<(a*b)<<endl;
+    // cout<<(a/b)<<endl;
+    // cout<<(a%b)<<endl;
+
+    //Relational Operaator
+    //These will return o or 1 on the condition
+    // cout<<(10>5)<<endl;
+    // cout<<(10<5)<<endl;
+    // cout<<(10<=5)<<endl;
+    // cout<<(10>=5)<<endl;
+    // cout<<(10==5)<<endl;
+    // cout<<(10!=5)<<endl;
+
+    //Logical operator(&&, ||, !)
+    // bool cond1 = true;
+    // bool cond2 = true;
+    // bool cond3 = false;
+
+    // if (cond1 || cond2 || cond3){
+    //     cout <<"Any one case is true."<<endl;
+    // }
+    // else {cout<<"All cases are not true"<<endl;}
+
+    //Assignment Operator
+    // int a = 5;
+    // cout <<a<<endl;
+
+    // a +=10;
+    // cout<<a<<endl;
+
+    // a -=10;
+    // cout<<a<<endl;
+
+    // a *=10;
+    // cout<<a<<endl;
+
+    // a /=10;
+    // cout<<a<<endl;
+
+    //Bitwise operator
+    //( & , | , ~ , << , >> , ^) 
+    // cout<<(5&4)<<endl;
+
+
+    return 0;
+}
